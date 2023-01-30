0acef4f8e943ec8936492149ad1878df701de510
Adil <adil.souirite@gmail.com>
2023-01-30 14:09:20 +0000

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,12 +3,12 @@ from pathlib import Path
 import shutil
 
 class Parser():
-    extensions = List[str]
+    extensions: List[str]= []
 
     def valid_extension(self, extension):
         return extension in self.extensions
     
-    def parse(self, path, source, dest):
+    def parse(self, path:Path, source:Path, dest:Path):
         raise NotImplementedError
     
     def read(self, path):
