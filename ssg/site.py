6f072daf9562c180ae1c26628dab8f3b2d4f41fe
Adil <adil.souirite@gmail.com>
2023-01-30 12:12:26 +0000

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
+
 class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
-    
+
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
@@ -14,4 +15,3 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
