a2502199bbe1e6d2e639bf300e4dd943d6c43e5d
Adil <adil.souirite@gmail.com>
2023-01-30 12:14:47 +0000

    Completed module

--- ssg.py
+++ ssg.py
@@ -7,6 +7,8 @@ def main(source="content", dest="dist"):
              "source": source,
              "dest": dest
          }
+         Site(**config).build()
 
-Site(**config).build()
+        
+
 typer.run(main)
